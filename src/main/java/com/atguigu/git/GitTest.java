6c30cfbf0e8c6a660db7e63a1a2d2c20e943805c
Oliver <Dream_sec@163.com>
2022-11-03 19:22:10 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master commit2");
         System.out.println("hot-fix commit");
+        System.out.println("push commit");
+
     }
 }
